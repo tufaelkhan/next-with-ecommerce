d011db94965da19ce112e3d605c89a75e5cc7398
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-07 03:43:00 -0700

    add styel

---
+++ src/app/add-product/page.tsx
@@ -0,0 +1,10 @@
+export default function AddProductPage(){
+    return (
+        <div>
+            <h1 className="text-4xl mt-5 mb-5 font-bold">Add product</h1>
+            <form>
+                <input className="mb-3 w-full " type="text" name="" id="" />
+            </form>
+        </div>
+    )
+}
