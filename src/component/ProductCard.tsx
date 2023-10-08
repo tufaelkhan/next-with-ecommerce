e77a73396231678cf4f5b8c34a33b009a7aaf465
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-08 11:07:01 -0700

    render product

--- src/component/ProductCard.tsx
+++ src/component/ProductCard.tsx
@@ -8,15 +8,17 @@ interface ProductCardProps{
 }
 
 export default function ProductCard({product}: ProductCardProps){
+    const isNew = Date.now() - new Date(product.createdAt).getTime() < 1000*60*60*24*7;
     return (
         <Link href={"/products" + product.id} className="card w-full bg-base-100 hover:shadow-xl transition duration-300 ">
             <figure>
-                <Image src={product?.image} alt={product?.name}/>
+                <Image src={product?.image} alt={product?.name} width={800} height={400} className="h-48 object-cover"/>
             </figure>
             <div className="card-body">
                 <h2 className="card-title">
                     {product?.name}
                 </h2>
+                    {isNew && <div className="badge badge-secondary">NEW</div>}
                 <p className="">{product?.description}</p>
                 <PriceTag price={product?.price}/>
             </div>
