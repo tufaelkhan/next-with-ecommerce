9657a944e4aa2fcce7b8d32e2d044bfe78d60878
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-08 12:05:10 -0700

    add title

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,7 +1,9 @@
 import PriceTag from "@/component/PriceTag"
 import { prisma } from "@/lib/db/prisma"
+import { Metadata } from "next"
 import Image from "next/image"
 import { notFound } from "next/navigation"
+import { cache } from "react"
 
 interface ProductPageProps {
     params: {
@@ -9,11 +11,26 @@ interface ProductPageProps {
     }
 }
 
-export default async function ProductPage({params: {id}}: ProductPageProps) {
+const getProduct = cache(async (id: string)=> {
     const product = await prisma.product.findUnique({where:{id}})
     if(!product) notFound()
+    return product;
+})
+export async function generateMetadata({params: {id}}: ProductPageProps): Promise<Metadata> {
+    const product = await getProduct(id)
+    return {
+        title: product.name + "e-commerce",
+        description: product.description,
+        openGraph:{
+            images: [{url: product.image}]
+        }
+    }
+}
+
+export default async function ProductPage({params: {id}}: ProductPageProps) {
+    const product = await getProduct(id)
     return (
-        <div className="flex flex-col lg:flex-row">
+        <div className="flex flex-col lg:flex-row gap-4 lg:items-center">
             <Image src={product.image} alt={product.name} width={500} height={500} className="rounded-lg" priority/>
             <div>
                 <h2 className="text-5xl font-bold">{product.name}</h2>
