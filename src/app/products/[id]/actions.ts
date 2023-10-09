9cff82644496ef3e11ca6d28e99a043eb871f92f
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-09 11:27:19 -0700

    add to db

--- src/app/products/[id]/actions.ts
+++ src/app/products/[id]/actions.ts
@@ -1,5 +1,7 @@
 "use server"
 
+import { createCart, getCart } from "@/lib/db/cart"
+
 export async function incrementProductQuantity(productId: string) {
-    
+    const cart = (await getCart()) ?? (await createCart())
 }
--- src/lib/db/cart.ts
+++ src/lib/db/cart.ts
@@ -1,18 +1,46 @@
 import { prisma } from "./prisma";
 import {cookies} "next/dist/client/components/headers"
+import {Cart, Prisma} from "@prisma/client"
 
-export async function getCart( ) {
+export type CartWithProduct = Prisma.CartGetPayload<{
+    include: {items: {include: {product: true}}}
+}>
+
+export type ShoppingCart = Cart  & {
+    size: number,
+    subtotal: number,
+}
+
+export async function getCart( ): Promise<ShoppingCart | null> {
     const localCartId = cookies().get("localCartId")?.value;
     const cart = localCartId ?
     await prisma.cart.findUnique({
-        where: {id: localCartId}
+        where: {id: localCartId},
+        include: {items: {include: {product: true}}}
     })
+    : null;
+
+    if(!cart){
+        return null;
+    }
+    return {
+        ...cart,
+        size: cart.items.reduce((acc, item) => acc + item.quantity, 0 ),
+        subtotal: cart.items.reduce((acc, item)=> acc + item.quantity * item.product.price, 0),
+    }
 }
 
-export async function createCart( ) {
+export async function createCart( ): Promise {
     const newCart = await prisma.cart.create({
         data:{}
     })
     
     cookies().set("localCartId", newCart.id)
+
+    return {
+        ...newCart,
+        items: [],
+        size: 0,
+        subtotal: 0,
+    }
 }
