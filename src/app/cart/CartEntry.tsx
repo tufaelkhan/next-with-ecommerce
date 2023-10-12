d97083e1a9a6499c066e36ff62d5ffcb018bfaea
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-11 18:41:57 -0700

    add loading

--- src/app/cart/CartEntry.tsx
+++ src/app/cart/CartEntry.tsx
@@ -4,12 +4,14 @@ import { CartItemWithProduct } from "@/lib/db/cart"
 import Image from "next/image"
 import Link from "next/link"
 import formatPrice from "@/lib/Format"
+import {useTransition} from "react"
 
 interface CartEntryProps{
     cartItem: CartItemWithProduct,
     setProductQuantity: ( productId: string, quantity: number) => Promise<vaid>
 }
 export default function CartEntry({cartItem : {product, quantity}, setProductQuantity }: CartEntryProps){
+    const [ isPending, startTransition ] = useTransition()
     const quantityOptions : JSX.Element[] = []
     for (let i = 1; i <= 99; i++){
         quantityOptions.push(<option value={i} key={i}>{i}</option>)
@@ -28,11 +30,17 @@ export default function CartEntry({cartItem : {product, quantity}, setProductQua
                     <select className="select select-bordered w-full max-w-[800px]" defaultValue={quantity}
                     onChange={e => {
                         const newQuantity = parseInt(e.currentTarget.value)
+                        startTransition(async ()=>{
+                            await setProductQuantity(product.id, newQuantity)
+                        })
                     }}
                     >{quantityOptions}</select>
                 </div>
                 <div className="flex items-center gap-2">
                     Total: {formatPrice(product.price * quantity)}
+                {isPending && (
+                    <span className="loading loading-spinner loading-sm"/>
+                )}
                 </div>
             </div>
             <div className="divider"/>
