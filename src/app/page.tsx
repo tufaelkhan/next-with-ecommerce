aec69040db5288f828374065225f870183a37534
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-15 18:24:42 -0700

    add pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,27 @@ import { prisma } from "@/lib/db/prisma"
 import Image from "next/image"
 import Link from "next/link"
 
+interface HomeProps{
+  searchParams: { page: string}
+}
 
+export default async function Home({searchParams: {page = "1"}}: HomeProps) {
+  const currentPage = parseInt(page)
 
-export default async function Home() {
+  const pageSize = 6
+  const heroItemCart = 1
+
+  const totalItemCart = await prisma.product.count();
+  const totalPages = Math.ceil((totalItemCart - heroItemCart) / pageSize)
+
   const products = await prisma.product.findMany({
-    orderBy: {id: "desc"}
+    orderBy: {id: "desc"},
+    skip: (currentPage - 1) * pageSize + (currentPage === 1 ? 0 : heroItemCart),
+    take: pageSize + (currentPage === 1 ? heroItemCart : 0)
   })
   return (
     <div className="flex flex-col items-center ">
+      { currentPage === 1 && 
       <div className="hero rounded-xl bg-base-200">
         <div className="hero-content flex-col lg:flex-row">
       <Image src={products[0].image} alt={products[0].name}
@@ -27,14 +40,18 @@ export default async function Home() {
       </div>
         </div>
       </div>
+      }
       <div className="my-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        
     {
-      products.slice(1).map(product =>(
+      (currentPage === 1 ? products.slice(1) : products).map(product =>(
         <ProductCard product={product} key={product.id}/>
       ))
     }
       </div>
-      <PaginationBar currentPage={13} totalPage={99}/>
+      { totalPages > 1 &&
+      <PaginationBar currentPage={currentPage} totalPage={totalPages}/>
+      }
     </div>
   )
 }
