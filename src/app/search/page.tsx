9f13d82d574c10502c966b6f57b427bd912e4c2b
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-15 18:43:38 -0700

    add page

---
+++ src/app/search/page.tsx
@@ -0,0 +1,29 @@
+import ProductCard from "@/component/ProductCard"
+import { prisma } from "@/lib/db/prisma"
+
+interface SearchPageProps{
+    searchParams: { query: string}
+}
+
+
+export default async function SearchPage({searchParams: {query}}: SearchPageProps){
+    const products = await prisma.product.findMany({
+        where: {
+            OR: [
+                {name: {contains: query, mode: "insensitive"}},
+                {description: {contains: query, mode: "insensitive"}},
+            ]
+        },
+        orderBy: {id: "desc"}
+    })
+    if(products.length === 0){
+        return <div className="text-center">No Product Found</div>
+    }
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {products.map(product =>(
+                <ProductCard product={product} key={product.id}/>
+            ))}
+        </div>
+    )
+}
